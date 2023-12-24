35055e9ccb17e4d7992acd2a5a40605aa3578763
HanRachid <handaouirachid@gmail.com>
2023-12-24 20:15:02 +0100

    fix port

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',strict_slashes=False)
 def index():
 
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
+
