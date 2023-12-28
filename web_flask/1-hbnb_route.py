8ff81102b75266c6ac27f08563803efe42944402
HanRachid <handaouirachid@gmail.com>
2023-12-28 12:43:08 +0100

    add task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Starts Falsk server, listening on route /
-displays Hello HBNH
+Starts flask server listening on route / and
+/HBNB
 """
 from flask import Flask
 app = Flask(__name__)
@@ -9,13 +9,10 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    return "Hello HBNB!"
 
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return "HBNB!"
+    return ""
 
 
 if __name__ == '__main__':
+
     app.run(host="0.0.0.0")
