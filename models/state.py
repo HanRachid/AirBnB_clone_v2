8cb478b6f74b65bbe1fd36426107951b8b8a89a1
HanRachid <handaouirachid@gmail.com>
2024-01-01 23:21:25 +0100

    fix pb with task 8

--- models/state.py
+++ models/state.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+from os import getenv
+
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+
+import models
+from models.base_model import Base, BaseModel
+from models.city import City
+
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade="all, delete", backref="state")
+
+    HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
+    if (HBNB_TYPE_STORAGE == 'db'):
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
